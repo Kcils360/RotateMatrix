f1a2f61c130c359c847dd4a5b26312df23736a25
Gregory Dukes <kcils360@gmail.com>
2017-11-02 16:44:15 -0700

    implimented rotateMatrix method that we found

--- RoateMatrix/RoateMatrix/Program.cs
+++ RoateMatrix/RoateMatrix/Program.cs
@@ -6,6 +6,28 @@ namespace RoateMatrix
     {
         static void Main(string[] args)
         {
+            int[,] array = new int[4, 4] {
+                { 1,2,3,4 },
+                { 5,6,7,8 },
+                { 9,0,11,12 },
+                { 13,14,15,16 }
+            };
+
+            int[,] rotated = RotateMatrix(array, 4);
+
+            int[,] RotateMatrix(int[,] matrix, int n)
+            {
+                int[,] ret = new int[n, n];
+
+                for (int i = 0; i < n; ++i)
+                {
+                    for (int j = 0; j < n; ++j)
+                    {
+                        ret[i, j] = matrix[n - j - 1, i];
+                    }
+                }
+                return ret;
+            }
 
         }
     }
